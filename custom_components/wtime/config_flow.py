ba8b119cd05fcc0ea33b2551eb042d48cb1f1533
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-22 01:50:38 -0500

    Update config_flow.py

--- custom_components/wtime/config_flow.py
+++ custom_components/wtime/config_flow.py
@@ -9,12 +9,15 @@ class WtimeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
+        # Prevent multiple configurations of the same integration
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
 
+        # Create an entry if the user submits input
         if user_input is not None:
             return self.async_create_entry(title="Wtime", data={})
 
+        # Otherwise, show the initial form
         return self.async_show_form(step_id="user")
 
     @staticmethod
