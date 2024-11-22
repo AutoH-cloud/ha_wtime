405f7db25210c1c260783d67c78bbe328b001e65
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-22 01:55:45 -0500

    Update config_flow.py

--- custom_components/wtime/config_flow.py
+++ custom_components/wtime/config_flow.py
@@ -1,41 +1,12 @@
 from homeassistant import config_entries
-from homeassistant.core import callback
-from .const import DOMAIN
+from . import DOMAIN
 
 class WtimeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Wtime."""
 
-    VERSION = 1
-
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
-        # Prevent multiple configurations of the same integration
-        if self._async_current_entries():
-            return self.async_abort(reason="single_instance_allowed")
-
-        # Create an entry if the user submits input
         if user_input is not None:
             return self.async_create_entry(title="Wtime", data={})
 
-        # Otherwise, show the initial form
         return self.async_show_form(step_id="user")
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
-        """Get the options flow for this handler."""
-        return WtimeOptionsFlowHandler(config_entry)
-
-
-class WtimeOptionsFlowHandler(config_entries.OptionsFlow):
-    """Handle an options flow for Wtime."""
-
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-        """Manage the options."""
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-
-        return self.async_show_form(step_id="init", data_schema=None)
