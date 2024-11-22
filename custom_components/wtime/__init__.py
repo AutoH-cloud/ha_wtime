ca31dd41fbaa1f8ca3e418daf457354948c0aa8d
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-22 01:05:08 -0500

    Create __init__.py

---
+++ custom_components/wtime/__init__.py
@@ -0,0 +1,20 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+DOMAIN = "wtime"
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up Wtime from configuration.yaml (not used for UI)."""
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Wtime from a config entry."""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload Wtime config entry."""
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return True
