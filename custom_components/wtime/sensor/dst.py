a263fd8d35ffc17a1bc26b258e0e514d601147da
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-22 02:11:52 -0500

    Update dst.py

--- custom_components/wtime/sensor/dst.py
+++ custom_components/wtime/sensor/dst.py
@@ -1,1 +1,23 @@
+from homeassistant.helpers.entity import Entity
+from datetime import datetime
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Set up the DST sensor."""
+    async_add_entities([DSTSensor()])
+
+
+class DSTSensor(Entity):
+    """Sensor to check if DST is active."""
+
+    def __init__(self):
+        self._state = None
+        self._name = "DST Status"
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def state(self):
+        return "After DST" if datetime.now().timetuple().tm_isdst == 1 else "Before DST"
 
