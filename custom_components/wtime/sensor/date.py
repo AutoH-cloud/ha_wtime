c5b5cc36bd554afb3dc1b70d08d5434869588b14
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-23 18:43:13 -0500

    Update date.py

--- custom_components/wtime/sensor/date.py
+++ custom_components/wtime/sensor/date.py
@@ -1,24 +1,20 @@
 from datetime import datetime
-from homeassistant.helpers.entity import Entity
-from homeassistant.const import DATE_FORMAT
+from homeassistant.components.sensor import SensorEntity
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the Date sensor."""
     async_add_entities([DateSensor()])
 
 
-class DateSensor(Entity):
+class DateSensor(SensorEntity):
     """Sensor to display the current date in various formats."""
 
     def __init__(self):
-        self._state = None
-        self._name = "Date"
+        """Initialize the Date sensor."""
+        self._attr_name = "Date"
+        self._attr_native_value = None
 
     @property
-    def name(self):
-        return self._name
-
-    @property
-    def state(self):
+    def native_value(self):
+        """Return the current date."""
         return datetime.now().strftime('%B %d, %Y')  # Current date in full format
-
