049cc2a8a3cf9992ac10d777a75df112dad40c58
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-23 18:50:45 -0500

    Update const.py

--- custom_components/wtime/const.py
+++ custom_components/wtime/const.py
@@ -1,2 +1,26 @@
+# const.py
+
+# Domain for the integration
 DOMAIN = "wtime"
 
+# Default settings for the integration (if any)
+DEFAULT_NAME = "Wtime"
+
+# Example of default sensor configurations
+DEFAULT_SENSORS = ["date", "time", "dst", "jewish_weekday"]
+
+# Translation keys
+CONF_SENSORS = "sensors"
+
+# Supported sensors (can be extended based on your custom sensors)
+SENSOR_TYPES = {
+    "date": "Date",
+    "time": "Time",
+    "dst": "DST Status",
+    "jewish_weekday": "Jewish Weekday",
+    "jewish_weekday_full": "Jewish Full Weekday",
+}
+
+# Some other constants that might be useful
+CONF_UPDATE_INTERVAL = "update_interval"
+DEFAULT_UPDATE_INTERVAL = 60  # in seconds
