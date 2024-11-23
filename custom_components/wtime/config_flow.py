90987b00755c9ac725d9efb35773dc89ac5e4114
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-23 18:50:14 -0500

    Update config_flow.py

--- custom_components/wtime/config_flow.py
+++ custom_components/wtime/config_flow.py
@@ -7,8 +7,14 @@ class WtimeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        """Handle the user input during configuration."""
+        # Check if an entry already exists, abort if so
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
+        
+        # Show the user form if no input is provided
         if user_input is None:
             return self.async_show_form(step_id="user")
+        
+        # Create an entry when user input is received
         return self.async_create_entry(title="Wtime", data={})
