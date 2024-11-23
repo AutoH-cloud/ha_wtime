b4c938bab41743b5bc28689d6c34ded90d12fc4d
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-23 18:48:17 -0500

    Update time.py

--- custom_components/wtime/sensor/time.py
+++ custom_components/wtime/sensor/time.py
@@ -1,23 +1,19 @@
 from datetime import datetime
-from homeassistant.helpers.entity import Entity
+from homeassistant.components.sensor import SensorEntity
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the Time sensor."""
     async_add_entities([TimeSensor()])
 
 
-class TimeSensor(Entity):
+class TimeSensor(SensorEntity):
     """Sensor to display the current time."""
 
     def __init__(self):
-        self._state = None
-        self._name = "Time"
+        """Initialize the sensor."""
+        self._attr_name = "Time"
 
     @property
-    def name(self):
-        return self._name
-
-    @property
-    def state(self):
-        return datetime.now().strftime('%I:%M %p')  # Current time in 12-hour format
-
+    def native_value(self):
+        """Return the current time in 12-hour format."""
+        return datetime.now().strftime('%I:%M %p')
