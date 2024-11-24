9736f67eae7246a72af38712e8274df4b727710d
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-24 01:21:46 -0500

    Add files via upload

---
+++ custom_components/wtime/binary_sensor.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from homeassistant.components.binary_sensor import BinarySensorEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import EntityCategory
+
+DOMAIN = "wtime"
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
+    """Set up Wtime binary sensors."""
+    async_add_entities([WTimeDSTBinarySensor(entry.entry_id)])
+
+class WTimeDSTBinarySensor(BinarySensorEntity):
+    """Representation of a DST sensor."""
+
+    def __init__(self, entry_id):
+        self._attr_name = "DST Status"
+        self._attr_unique_id = f"{entry_id}_dst_status"
+        self._attr_entity_category = EntityCategory.DIAGNOSTIC
+        self._state = None
+
+    @property
+    def is_on(self):
+        """Return true if the DST status is on."""
+        return self._state
+
+    async def async_update(self):
+        """Update the binary sensor state."""
+        now = datetime.now()
+        self._state = now.dst() is not None
