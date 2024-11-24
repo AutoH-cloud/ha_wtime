21e9c0ab5bf090bc2ea003c0b572f5c3751ee85f
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-24 02:32:15 -0500

    Update const.py

--- custom_components/wtime/const.py
+++ custom_components/wtime/const.py
@@ -10,7 +10,6 @@ SENSOR_TYPES = {
     "wtime_dst": "DST Status",
 }
 
-# Configuration keys
 CONF_SENSORS = "sensors"
 CONF_UPDATE_INTERVAL = "update_interval"
 DEFAULT_UPDATE_INTERVAL = 60  # Default update interval in seconds
