d2852d6fee13c2a941052d184e7d8ee0da769566
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-24 02:43:36 -0500

    Update sensor.py

--- custom_components/wtime/sensor.py
+++ custom_components/wtime/sensor.py
@@ -1,25 +1,41 @@
-# custom_components/WTime/sensor.py
-import logging
 from datetime import datetime
 from homeassistant.components.sensor import SensorEntity
+from homeassistant.components.binary_sensor import BinarySensorEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from .const import SENSORS
 
-_LOGGER = logging.getLogger(__name__)
+DOMAIN = "wtime"
+
+SENSORS = {
+    "date": {"format": "%B %d, %Y", "icon": "mdi:calendar"},
+    "date_numbers": {"format": "%x", "icon": "mdi:numeric"},
+    "time": {"format": "%-I:%M %p", "icon": "mdi:clock"},
+    "date_time": {"format": "%B %d, %Y - %-I:%M %p", "icon": "mdi:calendar-clock"},
+    "week_day_long": {"format": "%A", "icon": "mdi:calendar-today"},
+    "week_day_short": {"format": "%a", "icon": "mdi:calendar-today"},
+    "week_and_date": {"format": "%a, %B %d, %Y", "icon": "mdi:calendar-range"},
+    "jewish_week_date": {"format": None, "icon": "mdi:star-david"},
+    "jewish_week_date_full": {"format": None, "icon": "mdi:star-david"},
+    "current_month": {"format": "%B", "icon": "mdi:calendar-month"},
+    "current_season": {"format": None, "icon": "mdi:weather-partly-cloudy"},  # New season sensor
+}
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
-    """Set up WTime sensors."""
-    _LOGGER.debug("Setting up WTime sensors")
+    """Set up Wtime sensors and binary sensors."""
+    # Add regular sensors
     async_add_entities(
-        WTimeSensor(name, data, entry.entry_id) for name, data in SENSORS.items()
+        WtimeSensor(name, data, entry.entry_id) for name, data in SENSORS.items()
     )
 
-class WTimeSensor(SensorEntity):
-    """Representation of a WTime sensor."""
+    # Add DST binary sensor
+    async_add_entities([DSTBinarySensor(entry.entry_id)])
+
+
+class WtimeSensor(SensorEntity):
+    """Representation of a Wtime sensor."""
 
     def __init__(self, name, data, entry_id):
-        self._attr_name = name.replace("_", " ").title()  # Set name with spaces
+        self._attr_name = name.replace("_", " ").title()
         self._attr_unique_id = f"{entry_id}_{name}"
         self._format = data["format"]
         self._attr_icon = data["icon"]
@@ -30,12 +46,19 @@ class WTimeSensor(SensorEntity):
         """Return the state of the sensor."""
         jewish_weekdays = ["א", "ב", "ג", "ד", "ה", "ו", "שבת"]
         jewish_weekdays_full = [
-            "זונטאג", "מאנטאג", "דינסטאג", "מיטוואך", "דאנערשטיג", "פרייטאג", "שבת קודש",
+            "זונטאג",
+            "מאנטאג",
+            "דינסטאג",
+            "מיטוואך",
+            "דאנערשטיג",
+            "פרייטאג",
+            "שבת קודש",
         ]
         months = [
-            "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December",
         ]
-        seasons = ["Winter", "Spring", "Summer", "Fall"]
+        seasons = ["Winter", "Spring", "Summer", "Autumn"]
         weekdays_short = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
         weekdays_long = [
             "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",
@@ -43,7 +66,7 @@ class WTimeSensor(SensorEntity):
 
         now = datetime.now()
         month = now.month
-        weekday = (now.weekday() + 1) % 7  # Adjust for Sunday as the start of the week.
+        weekday = now.weekday()
 
         # Determine the current season based on the month
         if month in [12, 1, 2]:
@@ -53,22 +76,19 @@ class WTimeSensor(SensorEntity):
         elif month in [6, 7, 8]:
             season = "Summer"
         else:
-            season = "Fall"
+            season = "Autumn"
 
-        _LOGGER.debug(f"Returning state for {self._attr_name}")
-
-        # Return the correct value based on the sensor type
-        if self._attr_name == "Wtime Jewish Week Date":
+        if self._attr_name == "Jewish Week Date":
             return jewish_weekdays[weekday]
-        elif self._attr_name == "Wtime Jewish Week Date Full":
+        elif self._attr_name == "Jewish Week Date Full":
             return jewish_weekdays_full[weekday]
-        elif self._attr_name == "Wtime Week Day Long":
+        elif self._attr_name == "Week Day Long":
             return weekdays_long[weekday]
-        elif self._attr_name == "Wtime Week Day Short":
+        elif self._attr_name == "Week Day Short":
             return weekdays_short[weekday]
-        elif self._attr_name == "Wtime Current Month":
+        elif self._attr_name == "Current Month":
             return months[month - 1]
-        elif self._attr_name == "Wtime Current Season":
+        elif self._attr_name == "Current Season":
             return season
         else:
             return now.strftime(self._format)
@@ -78,31 +98,65 @@ class WTimeSensor(SensorEntity):
         """Return additional attributes for dropdown support."""
         jewish_weekdays = ["א", "ב", "ג", "ד", "ה", "ו", "שבת"]
         jewish_weekdays_full = [
-            "זונטאג", "מאנטאג", "דינסטאג", "מיטוואך", "דאנערשטיג", "פרייטאג", "שבת קודש",
+            "זונטאג",
+            "מאנטאג",
+            "דינסטאג",
+            "מיטוואך",
+            "דאנערשטיג",
+            "פרייטאג",
+            "שבת קודש",
         ]
         months = [
-            "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December",
         ]
-        seasons = ["Winter", "Spring", "Summer", "Fall"]
+        seasons = ["Winter", "Spring", "Summer", "Autumn"]
         weekdays_short = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
         weekdays_long = [
             "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",
         ]
 
-        if self._attr_name == "WTime Jewish Week Date":
+        if self._attr_name == "Jewish Week Date":
             return {"options": jewish_weekdays}
-        elif self._attr_name == "WTime Jewish Week Date Full":
+        elif self._attr_name == "Jewish Week Date Full":
             return {"options": jewish_weekdays_full}
-        elif self._attr_name == "WTime Week Day Long":
+        elif self._attr_name == "Week Day Long":
             return {"options": weekdays_long}
-        elif self._attr_name == "WTime Week Day Short":
+        elif self._attr_name == "Week Day Short":
             return {"options": weekdays_short}
-        elif self._attr_name == "WTime Current Month":
+        elif self._attr_name == "Current Month":
             return {"options": months}
-        elif self._attr_name == "WTime Current Season":
+        elif self._attr_name == "Current Season":
             return {"options": seasons}
-        return {}
+        return None
 
     async def async_update(self):
         """Update the sensor state."""
         self._state = self.native_value
+
+
+class DSTBinarySensor(BinarySensorEntity):
+    """Representation of a binary sensor for DST status."""
+
+    def __init__(self, entry_id):
+        self._attr_name = "DST Status"
+        self._attr_unique_id = f"{entry_id}_dst_status"
+        self._attr_icon = "mdi:clock-alert"
+        self._state = None
+
+    @property
+    def is_on(self):
+        """Return True if DST is active, False otherwise."""
+        return datetime.now().timetuple().tm_isdst == 1
+
+    @property
+    def extra_state_attributes(self):
+        """Return additional attributes for the DST sensor."""
+        return {
+            "description": "Indicates whether the system is currently in Daylight Savings Time.",
+            "active": "DST is currently active." if self.is_on else "DST is not active.",
+        }
+
+    async def async_update(self):
+        """Update the binary sensor state."""
+        self._state = self.is_on
