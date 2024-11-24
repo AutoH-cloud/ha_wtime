79ef812213e773d7300471128c0374d141101cf3
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-24 02:35:54 -0500

    Update __init__.py

--- custom_components/wtime/__init__.py
+++ custom_components/wtime/__init__.py
@@ -2,21 +2,23 @@
 
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.helpers.typing import ConfigType
+from .const import DOMAIN
 
-DOMAIN = "wtime"
+PLATFORMS = ["sensor", "binary_sensor"]
 
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the WTime component."""
-    hass.data[DOMAIN] = {}
+    hass.data.setdefault(DOMAIN, {})
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up WTime from a config entry."""
-    hass.config_entries.async_setup_platforms(entry, ["sensor", "binary_sensor"])
+    hass.data[DOMAIN][entry.entry_id] = entry
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a WTime config entry."""
-    hass.config_entries.async_unload_platforms(entry, ["sensor", "binary_sensor"])
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    hass.data[DOMAIN].pop(entry.entry_id)
     return True
