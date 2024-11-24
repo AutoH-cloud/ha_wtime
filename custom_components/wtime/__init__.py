4ba12f58ddaa09c50ad4f213e0eee124561f607b
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-24 02:01:02 -0500

    Update __init__.py

--- custom_components/wtime/__init__.py
+++ custom_components/wtime/__init__.py
@@ -1,19 +1,11 @@
-from homeassistant import config_entries
+# custom_components/WTime/__init__.py
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from .sensor import async_setup_entry as setup_sensors  # Import the async_setup_entry from the sensor module
+import logging
 
 _LOGGER = logging.getLogger(__name__)
 
-DOMAIN = "wtime"
-
 async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the WTime integration."""
-    _LOGGER.debug("Setting up Wtime integration")
-    return True
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up WTime from a config entry."""
-    _LOGGER.debug("Setting up Wtime entry")
-    # Ensure you pass async_add_entities to the sensor setup
-    await setup_sensors(hass, entry, hass.helpers.entity_platform.async_add_entities)
+    """Set up the WTime component."""
+    _LOGGER.debug("Setting up WTime integration")
     return True
