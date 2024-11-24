d339834a0162fd1dca35dc75b15a32db93083146
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-24 02:09:27 -0500

    Update __init__.py

--- custom_components/wtime/__init__.py
+++ custom_components/wtime/__init__.py
@@ -1,11 +1,1 @@
-# custom_components/WTime/__init__.py
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-import logging
 
-_LOGGER = logging.getLogger(__name__)
-
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the WTime component."""
-    _LOGGER.debug("Setting up WTime integration")
-    return True
