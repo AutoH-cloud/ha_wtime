55bb8cc839a1eb6be0bdd1bf9c25aa80771d59e5
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-24 01:02:59 -0500

    Update __init__.py

--- custom_components/wtime/__init__.py
+++ custom_components/wtime/__init__.py
@@ -1,17 +1,20 @@
-"""The Wtime integration."""
-
+import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import discovery
+
+_LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "wtime"
 
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the WTime integration."""
+    _LOGGER.debug("Setting up Wtime integration")
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up WTime from a config entry."""
+    _LOGGER.debug("Setting up Wtime entry")
     from .sensor import async_setup_entry as setup_sensors
     await setup_sensors(hass, entry, async_add_entities)
     return True
