343e85b0f1a7e60a29429017c1b37ad54dffdffe
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-24 00:59:58 -0500

    Update __init__.py

--- custom_components/wtime/__init__.py
+++ custom_components/wtime/__init__.py
@@ -8,12 +8,10 @@ DOMAIN = "wtime"
 
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the WTime integration."""
-    # This would allow the discovery process to be handled
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up WTime from a config entry."""
-    # Register your sensors here or call async_setup_entry for each sensor
     from .sensor import async_setup_entry as setup_sensors
     await setup_sensors(hass, entry, async_add_entities)
     return True
