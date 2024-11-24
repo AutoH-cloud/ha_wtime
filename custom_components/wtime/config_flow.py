f5b06a993c5e86d053a75b0a099b429e115cb388
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-24 02:27:52 -0500

    Update config_flow.py

--- custom_components/wtime/config_flow.py
+++ custom_components/wtime/config_flow.py
@@ -1,4 +1,3 @@
-# custom_components/WTime/config_flow.py
 from homeassistant import config_entries
 from .const import DOMAIN
 
@@ -9,13 +8,10 @@ class WTimeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the user input during configuration."""
-        # Check if an entry already exists, abort if so
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
-        
-        # Show the user form if no input is provided
+
         if user_input is None:
             return self.async_show_form(step_id="user")
-        
-        # Create an entry when user input is received
+
         return self.async_create_entry(title="WTime", data={})
