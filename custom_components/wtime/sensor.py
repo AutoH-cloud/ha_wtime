4a0a3a83bcacabecbb4136a4cb985ba1440053fb
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-24 12:24:48 -0500

    Update sensor.py

--- custom_components/wtime/sensor.py
+++ custom_components/wtime/sensor.py
@@ -12,7 +12,6 @@ SENSORS = {
     "wtime_week_day": {"format": "%A", "icon": "mdi:calendar-today"},
     "wtime_week_day_short": {"format": "%a", "icon": "mdi:calendar-today"},
     "wtime_jewish_week_date": {"format": None, "icon": "mdi:star-david"},
-    "wtime_jewish_week_date": {"format": None, "icon": "mdi:star-david"},
     "wtime_current_month": {"format": "%B", "icon": "mdi:calendar-month"},
     "wtime_current_season": {"format": None, "icon": "mdi:weather-partly-cloudy"},
 }
@@ -27,7 +26,8 @@ class WtimeSensor(SensorEntity):
     """Representation of a WTime sensor."""
 
     def __init__(self, name, data, entry_id):
-        self._attr_name = name.replace("_", " ").title()
+        # Update friendly name with "WTime" prefix
+        self._attr_name = f"WTime {name.replace('_', ' ').title()}"
         self._attr_unique_id = f"{entry_id}_{name}"
         self._format = data["format"]
         self._attr_icon = data["icon"]
@@ -50,7 +50,7 @@ class WtimeSensor(SensorEntity):
 
         now = datetime.now()
         month = now.month
-        weekday = (now.weekday() + 1) % 7  # Adjust for 1-day offset (Sunday = 0)
+        weekday = (now.weekday() + 1) % 7  # Adjust for Sunday start (corrects offset)
 
         # Determine the current season based on the month
         if month in [12, 1, 2]:
@@ -62,17 +62,15 @@ class WtimeSensor(SensorEntity):
         else:
             season = "Fall"
 
-        if self._attr_name == "Wtime Jewish Week Date":
+        if self._attr_name == "WTime Jewish Week Date":
             return jewish_weekdays_short[weekday]
-        elif self._attr_name == "Wtime Jewish Week Date":
-            return jewish_weekdays[weekday]
-        elif self._attr_name == "Wtime Week Day":
+        elif self._attr_name == "WTime Week Day":
             return weekdays_long[weekday]
-        elif self._attr_name == "Wtime Week Day Short":
+        elif self._attr_name == "WTime Week Day Short":
             return weekdays_short[weekday]
-        elif self._attr_name == "Wtime Current Month":
+        elif self._attr_name == "WTime Current Month":
             return months[month - 1]
-        elif self._attr_name == "Wtime Current Season":
+        elif self._attr_name == "WTime Current Season":
             return season
         else:
             return now.strftime(self._format)
@@ -92,17 +90,15 @@ class WtimeSensor(SensorEntity):
             "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",
         ]
 
-        if self._attr_name == "Wtime Jewish Week Date":
+        if self._attr_name == "WTime Jewish Week Date":
             return {"options": jewish_weekdays_short}
-        elif self._attr_name == "Wtime Jewish Week Date":
-            return {"options": jewish_weekdays}
-        elif self._attr_name == "Wtime Week Day":
+        elif self._attr_name == "WTime Week Day":
             return {"options": weekdays_long}
-        elif self._attr_name == "Wtime Week Day Short":
+        elif self._attr_name == "WTime Week Day Short":
             return {"options": weekdays_short}
-        elif self._attr_name == "Wtime Current Month":
+        elif self._attr_name == "WTime Current Month":
             return {"options": months}
-        elif self._attr_name == "Wtime Current Season":
+        elif self._attr_name == "WTime Current Season":
             return {"options": seasons}
         return None
 
