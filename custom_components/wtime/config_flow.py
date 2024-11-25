58893bce7d69ae3fe776899ef7ca019911205fc2
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-24 23:11:16 -0500

    Update config_flow.py

--- custom_components/wtime/config_flow.py
+++ custom_components/wtime/config_flow.py
@@ -7,6 +7,7 @@ class WTimeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        """Handle the user step."""
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
 
