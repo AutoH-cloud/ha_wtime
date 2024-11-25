6dea9511d3b913aae6f28bb7ce068a15d215c9ec
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-24 23:00:58 -0500

    Update __init__.py

--- custom_components/wtime/__init__.py
+++ custom_components/wtime/__init__.py
@@ -1,24 +1,15 @@
-"""WTime integration for Home Assistant."""
+"""Initialize the wdate integration."""
+import logging
 
 from homeassistant.core import HomeAssistant
-from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.typing import ConfigType
+
 from .const import DOMAIN
 
-PLATFORMS = ["sensor", "binary_sensor"]
+_LOGGER = logging.getLogger(__name__)
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the WTime component."""
-    hass.data.setdefault(DOMAIN, {})
-    return True
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up WTime from a config entry."""
-    hass.data[DOMAIN][entry.entry_id] = entry
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    return True
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Unload a config entry."""
-    await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    hass.data[DOMAIN].pop(entry.entry_id)
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the wdate integration."""
+    # Perform any necessary initialization here
+    _LOGGER.info("Setting up the wdate integration")
     return True
