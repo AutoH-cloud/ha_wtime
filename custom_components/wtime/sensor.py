d80fb6883b80e9e90e3f98f4c906b6bfd9fdf47e
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-24 20:58:35 -0500

    Update sensor.py

--- custom_components/wtime/sensor.py
+++ custom_components/wtime/sensor.py
@@ -12,8 +12,6 @@ SENSORS = {
     "wtime_time": {"format": "%-I:%M %p", "icon": "mdi:clock"},
     "wtime_week_day": {"format": "%A", "icon": "mdi:calendar-today"},
     "wtime_week_day_short": {"format": "%a", "icon": "mdi:calendar-today"},
-    "wtime_current_month": {"format": "%B", "icon": "mdi:calendar-month"},
-    "wtime_current_season": {"format": None, "icon": "mdi:weather-partly-cloudy"},
 }
 
 SEASON_OPTIONS = ["Winter", "Spring", "Summer", "Fall"]
@@ -25,13 +23,17 @@ MONTH_OPTIONS = [
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
     """Set up WTime sensors and selects."""
+    # Add sensors
     sensors = [
         WtimeSensor(name, data, entry.entry_id) for name, data in SENSORS.items()
     ]
+
+    # Add select entities for dropdowns
     selects = [
         WtimeSelect("Wtime Current Month", MONTH_OPTIONS, entry.entry_id),
         WtimeSelect("Wtime Current Season", SEASON_OPTIONS, entry.entry_id),
     ]
+
     async_add_entities(sensors + selects, update_before_add=True)
 
 
@@ -48,37 +50,13 @@ class WtimeSensor(SensorEntity):
     def native_value(self):
         """Return the state of the sensor."""
         now = datetime.now()
-        month = now.month
-
-        # Determine the current season based on the month
-        if month in [12, 1, 2]:
-            season = "Winter"
-        elif month in [3, 4, 5]:
-            season = "Spring"
-        elif month in [6, 7, 8]:
-            season = "Summer"
-        else:
-            season = "Fall"
 
         if self._attr_name == "Wtime Week Day":
             return now.strftime("%A")
         elif self._attr_name == "Wtime Week Day Short":
             return now.strftime("%a")
-        elif self._attr_name == "Wtime Current Month":
-            return now.strftime("%B")
-        elif self._attr_name == "Wtime Current Season":
-            return season
         else:
             return now.strftime(self._format)
-
-    @property
-    def extra_state_attributes(self):
-        """Provide extra attributes."""
-        if self._attr_name == "Wtime Current Month":
-            return {"options": MONTH_OPTIONS}
-        elif self._attr_name == "Wtime Current Season":
-            return {"options": SEASON_OPTIONS}
-        return None
 
     async def async_update(self):
         """Update the sensor state."""
@@ -94,6 +72,11 @@ class WtimeSelect(SelectEntity):
         self._options = options
         self._attr_options = options
         self._attr_current_option = options[0]
+
+    @property
+    def current_option(self):
+        """Return the currently selected option."""
+        return self._attr_current_option
 
     async def async_select_option(self, option: str):
         """Change the selected option."""
