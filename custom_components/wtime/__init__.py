cd03f21478fa8e6de995318c938320bb115d6407
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-24 23:10:56 -0500

    Update __init__.py

--- custom_components/wtime/__init__.py
+++ custom_components/wtime/__init__.py
@@ -1,15 +1,22 @@
-"""Initialize the wdate integration."""
+"""Initialize the wtime integration."""
 import logging
 
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
 
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up the wdate integration."""
-    # Perform any necessary initialization here
-    _LOGGER.info("Setting up the wdate integration")
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up WTime integration from a config entry."""
+    _LOGGER.info("Setting up WTime integration")
+    hass.data.setdefault(DOMAIN, {})
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload WTime integration."""
+    _LOGGER.info("Unloading WTime integration")
+    if DOMAIN in hass.data:
+        hass.data.pop(DOMAIN)
     return True
